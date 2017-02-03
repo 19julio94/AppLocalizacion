87ff3029197e31eea0e8ab8f99fdd5741f43a2b3
19julio94 <juliopatriciodasilva@gmail.com>
2017-02-03 02:04:50 +0100

    Metodos para calcular las distancias  y saber mi posicion

--- app/src/main/java/com/example/jp/localizacionapp/MapsActivity.java
+++ app/src/main/java/com/example/jp/localizacionapp/MapsActivity.java
@@ -1,42 +1,77 @@
 package com.example.jp.localizacionapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import layout.FirstMapFragment;
+import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,
+        GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks,GoogleMap.OnMapLongClickListener  {
 
     private static final int LOCATION_REQUEST_CODE = 1;
     private GoogleMap mMap;
-    private FirstMapFragment mFirstMapFragment;
+    private GoogleApiClient apiClient;
+    public static String result="";
+    public static String distancia;
+    public static double latpos, lngpos;
+    public static double latTele=42.236948;
+    public static double lngTele=-8.712725;
+    private static final String LOGTAG = "android-localizacion";
+    public static Marker marcaTelepizza;
+    LatLng center = new LatLng(42.236954, -8.712717);
+    int radius = 100;
+    public static double latAbn=42.237782;
+    public static double lngAbn=-8.720155;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mFirstMapFragment = FirstMapFragment.newInstance();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.map, mFirstMapFragment)
-                .commit();
-        // Registrar escucha onMapReadyCallback
-        mFirstMapFragment.getMapAsync(this);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
 
@@ -44,6 +79,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+
+        LatLng telepizza = new LatLng(latTele, lngTele);
+        marcaTelepizza = mMap.addMarker(new MarkerOptions().position(telepizza).title("Telepizza").snippet("Marca Telepizza"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(telepizza));
+        marcaTelepizza.setVisible(false);
+
+
 
         // Controles UI
         if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -63,50 +107,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        CircleOptions circleOptions = new CircleOptions()
+                .center(center)
+                .radius(radius)
+                .strokeColor(Color.parseColor("#0D47A1"))
+                .strokeWidth(4)
+                .fillColor(Color.argb(32, 33, 150, 243));
+        // Añadir círculo
+        Circle circle = mMap.addCircle(circleOptions);
 
-        // Marcadores
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
 
 
         //Anhadimops un marcador definiendo las coordenadas de la ciudad de vigo,ademas se pe ha puesto un titulo
 
-        LatLng telepizza = new LatLng(42.236948, -8.712725);
-        googleMap.addMarker(new MarkerOptions()
-                .position(telepizza)
-                .visible(true)
-                .title("Telepizza")
 
-        );
-
-        LatLng abanca = new LatLng(42.237782, -8.720155);
-        googleMap.addMarker(new MarkerOptions()
-                .position(abanca)
-                .visible(true)
-                .title("Abanca")
-
-        );
-
-        LatLng casadolibro = new LatLng(42.236857, -8.721821);
-        googleMap.addMarker(new MarkerOptions()
-                .position(casadolibro)
-                .visible(true)
-                .title("Casa do Libro")
-
-        );
-
-        LatLng fiveMonkeys = new LatLng(42.23806, -8.724938);
-        googleMap.addMarker(new MarkerOptions()
-                .position(fiveMonkeys)
-                .visible(true)
-                .title("Five Monkeys Cafe")
-
-        );
 
         LatLng vigo = new LatLng(42.2328200, -8.7226400);
         googleMap.addMarker(new MarkerOptions()
                 .position(vigo)
                 .visible(true)
-                .title("idade de Vigo")
+                .title("cidade de Vigo")
 
         );
 
@@ -143,4 +163,85 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
     }
-}
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+        updateUI(lastLocation);
+        calcularDistancia();
+
+        if(result.equals("SiguientePista")){
+            LatLng tesoro2 = new LatLng(latAbn, lngAbn);
+            latTele=latAbn;
+            lngTele=lngAbn;
+            marcaTelepizza.remove();
+            marcaTelepizza=mMap.addMarker(new MarkerOptions().position(tesoro2).title("Tesoro2").snippet("Marca Tesoro2").visible(false));
+
+        }
+
+        if(result.equals("HasGanado")){
+            Toast.makeText(this, "Has Ganado", Toast.LENGTH_LONG).show();
+        }
+
+        Toast.makeText(this, distancia+" metros ", Toast.LENGTH_LONG).show();
+
+    }
+    public void calcularDistancia() {
+        /*String la= String.valueOf(lat1);
+        String lo =String.valueOf(lng1);
+        Toast.makeText(this, la+" "+lo, Toast.LENGTH_LONG).show();*/
+
+        double earthRadius = 6372.795477598;
+
+        double dLat = Math.toRadians(latpos-latTele);
+        double dLng = Math.toRadians(lngpos-lngTele);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(latTele)) * Math.cos(Math.toRadians(latpos)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+        double distMet=dist*1000;
+        distancia=String.valueOf(distMet);
+
+
+
+        if(distMet<=20){
+            marcaTelepizza.setVisible(true);
+        }else {
+            marcaTelepizza.setVisible(false);
+        }
+
+
+
+    }
+    private void updateUI(Location loc) {
+
+        if (loc != null) {
+            latpos=loc.getLatitude();
+            lngpos=loc.getLongitude();
+            //Tosat para saber longitud y latitud de mi posicion
+            // Toast.makeText(this, String.valueOf(lat1)+" "+String.valueOf(lng1), Toast.LENGTH_LONG).show();
+        } else {
+
+            Toast.makeText(this, "Latitud y Longitud desconocidas", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+    }
+
+
+
+
